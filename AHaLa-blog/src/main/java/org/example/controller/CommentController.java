f75f147f508068367d67eb797f849d3089fbb648
AHaLa <taonengyi@hotmail.com>
2024-01-13 16:04:22 +0800

    swagger2

--- AHaLa-blog/src/main/java/org/example/controller/CommentController.java
+++ AHaLa-blog/src/main/java/org/example/controller/CommentController.java
@@ -1,14 +1,21 @@
 package org.example.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.example.constants.SystemConstants;
 import org.example.domain.ResponseResult;
+import org.example.domain.dto.AddCommentDto;
 import org.example.domain.entity.Comment;
 import org.example.service.CommentService;
+import org.example.utils.BeanCopyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("comment")
+@Api(tags="评论",description = "评论相关接口")
 public class CommentController {
     @Autowired
     private CommentService commentService;
@@ -18,12 +25,18 @@ public class CommentController {
     }
 
     @PostMapping
-    public ResponseResult addComment(@RequestBody Comment comment){
+    public ResponseResult addComment(@RequestBody AddCommentDto addCommentDto){
+        Comment comment = BeanCopyUtils.copyBean(addCommentDto, Comment.class);
         return commentService.addComment(comment);
 
     }
 
     @GetMapping("linkCommentList")
+    @ApiOperation(value="友链评论列表",notes = "获取一页友链评论")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum", value = "页号"),
+            @ApiImplicitParam(name = "pageSize", value = "页大小")
+    })
     public ResponseResult linkCommentList(Integer pageNum,Integer pageSize){
         return commentService.commentList(SystemConstants.LINK_COMMENT,null,pageNum,pageSize);
     }
